b405e8f64c7ccabe70c7f732e3852c91e82803b0
Steven Edwards <german3479@hotmail.com>
2023-06-09 13:17:02 -0700

    added last styles, added footer element to html

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const defSize = 16;
 
 const resetBtn = document.querySelector('.reset-btn');
 const gridContainer = document.querySelector('.grid-container')
+const typeBox = document.querySelector('.type-box');
+const timer = document.querySelector('.currentDate');
 
 let currentState = 'default';
 
@@ -9,13 +11,30 @@ function handleMouseOver(e){
     const item = e.target;
     switch (currentState) {
         case 'darken':
-            //code
+            let bgc = item.style.backgroundColor;
+
+            if (!bgc || bgc === 'null'){
+                bgc = 'rgb(238, 238, 238)';
+            }
+            const rgb = bgc.match(/\d+/g);
+            let [r, g, b] = rgb.map(Number);
+            
+            r *= 0.9;
+            g *= 0.9;
+            b *= 0.9;
+
+            item.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
             break;
         case 'rainbow':
-            //code
+            const rV = Math.floor(Math.random() * 256);
+            const gV = Math.floor(Math.random() * 256);
+            const bV = Math.floor(Math.random() * 256);
+
+            item.style.backgroundColor = `rgb(${rV}, ${gV}, ${bV})`;
             break;
         case 'color-pick':
-            //code;
+            const color = document.querySelector(".color-input").value;
+            item.style.backgroundColor = color;
             break;
         default:
             item.style.backgroundColor = "black";
@@ -52,3 +71,35 @@ resetBtn.addEventListener('click', () =>{
 
     createNewGrid(newSize)
 })
+typeBox.addEventListener('click', function(e){
+    const target = e.target;
+
+    switch (true) {
+        case target.classList.contains('darken-btn'):
+            currentState = 'darken';
+            break;
+        case target.classList.contains('rainbow-btn'):
+            currentState = 'rainbow';
+            break;
+        case target.classList.contains('color-input'):
+            currentState = 'color-pick';
+            break;
+        default:
+            break;
+    }
+})
+
+// Set time:
+function setTime(){
+    let currentDate = new Date()
+
+    let month = currentDate.toLocaleString('default', {month: 'short'})
+    let day = currentDate.getDate();
+    let year = currentDate.getFullYear();
+
+    let displayDate = `${month} ${day}, ${year}`;
+
+    timer.textContent = displayDate;
+}
+
+setTime()
