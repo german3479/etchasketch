01acafb348822c6f29420ecfbbdf7f861da81671
Steven Edwards <german3479@hotmail.com>
2023-06-09 11:25:51 -0700

    reworked logic until i got 'default' behavior working reliably

--- script.js
+++ script.js
@@ -1,114 +1,54 @@
-//Basic beginning values/variables
+const defSize = 16;
 
-const defVal = 16;
-let userColumns;
-let userRows;
-//Button captures
-const resetBtn = document.querySelector(".reset-btn");
-const darkenBtn = document.querySelector(".darken-btn");
-const rainbowBtn = document.querySelector(".rainbow-btn");
-const colorInput = document.querySelector(".color-input")
+const resetBtn = document.querySelector('.reset-btn');
+const gridContainer = document.querySelector('.grid-container')
 
-let color = "#000000"
+let currentState = 'default';
 
-//Grid setup
-const typeBox = document.querySelector(".type-box");
-const grid = document.querySelector(".grid-container");
-let gridItems;
-
-const changeColor = () => {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-
-    color = `rgb(${r}, ${g}, ${b})`
-}
-const darkenColor = (e) => {
-    gridItems.forEach((item) => {
-      item.removeEventListener("mouseover", changeColor);
-      item.addEventListener("mouseover", function () {
-        let currColor = this.style.backgroundColor;
-        if (currColor) {
-          if (currColor.startsWith("rgb")) {
-            currColor = hexToRgb(currColor);
-          } else {
-            currColor = currColor.trim();
-            currColor = hexToRgb(currColor);
-          }
-          let r = parseInt(currColor.substring(4, currColor.indexOf(","))) * 0.9;
-          let g = parseInt(
-            currColor.substring(currColor.indexOf(",") + 2, currColor.lastIndexOf(","))
-          ) * 0.9;
-          let b = parseInt(currColor.substring(currColor.lastIndexOf(",") + 2, currColor.length - 1)) * 0.9;
-          this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        }
-      });
-    });
-  };
-  
-
-
-const hexToRgb = (color) =>{
-    if (color.startsWith('rgb')){
-        return color;
-    } else if (color[0] === '#'){
-    const r = parseInt(color.substring(1,3), 16) 
-    const g = parseInt(color.substring(3,5), 16)
-    const b = parseInt(color.substring(5,7), 16)
-
-    return `rgb(${r}, ${g}, ${b})`
-}}
-
-let createGridItems = (dimension = defVal) => {
-    let childrenArray = Array.from(grid.children);
-
-    if (childrenArray.length){
-        childrenArray.forEach(child =>{
-            child.remove()
-        })
+function handleMouseOver(e){
+    const item = e.target;
+    switch (currentState) {
+        case 'darken':
+            //code
+            break;
+        case 'rainbow':
+            //code
+            break;
+        case 'color-pick':
+            //code;
+            break;
+        default:
+            item.style.backgroundColor = "black";
+            break;
     }
-    const numItems = dimension ** 2;
-    const containerWidth = grid.clientWidth;
-    const squareSize = containerWidth / dimension;
-
-    grid.style.gridTemplateColumns = `repeat(${dimension}, ${squareSize}px)`
-    grid.style.gridTemplateRows = `repeat(${dimension}, ${squareSize}px)`;
-
-    for (let i = 0; i < numItems; i++){
-        let newItem = document.createElement('div');
-        newItem.classList.add('grid-item')
-        grid.append(newItem);
-    }
-
-    gridItems = document.querySelectorAll(".grid-item");
 }
 
-createGridItems();
+function createNewGrid(size = defSize){
+    const totalItems = size**2
+
+    while (gridContainer.firstChild){
+        gridContainer.firstChild.remove()
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, minmax(0, 1fr))`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, minmax(0, 1fr))`;
+
+    for (let i = 0; i < totalItems; i++){
+        const item = document.createElement('div')
+        item.classList.add('grid-item');
+        item.addEventListener('mouseover', handleMouseOver)
+        gridContainer.appendChild(item);
+    }
+
+}
+
+
+createNewGrid()
 
 resetBtn.addEventListener('click', () =>{
-    let newAmnt = prompt("How many new items do you want?")
+    let newSize = parseInt(prompt('Enter new grid size: '));
 
-    createGridItems(newAmnt || 16)
-})
+    newSize = Math.min(newSize, 100);
 
-darkenBtn.addEventListener('click', () =>{
-    gridItems.forEach(item => {
-        item.removeEventListener('mouseover', changeColor)
-        item.addEventListener('mouseover', darkenColor)
-    })
-})
-rainbowBtn.addEventListener('click', () =>{
-    gridItems.forEach(item =>{
-        item.removeEventListener('mouseover', darkenColor)
-        item.addEventListener('mouseover', changeColor);
-    })
-})
-colorInput.addEventListener('change', e =>{
-    gridItems.forEach(item => item.removeEventListener('mouseover', changeColor))
-    color = e.target.value;
-})
-grid.addEventListener('mouseover', e => {
-    if (e.target.classList.contains("grid-item")){
-        e.target.style.backgroundColor = color;
-    }
+    createNewGrid(newSize)
 })
